7ce604e4453645c4c1ae25bf131176be42f295d5
Joathan <Joathan@DESKTOP-PM88QL6>
2017-05-23 15:55:26 -0300

    Implementação dos Angular JS
    Signed-off-by: Joathan <joathan@joathan.com.br>

--- src/main/java/br/com/joathan/agenda/interf/EquipamentoRepository.java
+++ src/main/java/br/com/joathan/agenda/interf/EquipamentoRepository.java
@@ -5,11 +5,12 @@ import java.util.List;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import br.com.joathan.agenda.model.Equipamento;
 
-@RepositoryRestResource(collectionResourceRel = "equipamento", path = "equipamento")
+//@RepositoryRestResource(collectionResourceRel = "equipamento", path = "equipamento")
 public interface EquipamentoRepository extends PagingAndSortingRepository<Equipamento, Long> {
-
-	List<Equipamento> findByDescricao(@Param("descricao") String descricao);
+	
+	//List<Equipamento> findByDescricao(@Param("descricao") String descricao);
 }
